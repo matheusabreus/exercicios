5bb8cf8ba314eff97a07b8c811baa047a8ae1fa1
matheusabreus <matheusabreus@hotmail.com>
2021-03-29 20:54:37 -0300

    Create SEMANA01_q06.c

---
+++ SEMANA01_q06.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    char b;
+    scanf("%d", &a);
+    if(a == 0){
+        printf("zero");
+    }
+    if(a == 1) {
+        printf("one");
+    }
+    if(a == 2) {
+        printf("two");
+    }
+    if(a == 3) {
+        printf("three");
+    }
+    if(a == 4) {
+        printf("four");
+    }
+    if(a == 5) {
+        printf("five");
+    }
+    if(a == 6) {
+        printf("six");
+    }
+    if(a == 7) {
+        printf("seven");
+    }
+    if(a == 8) {
+        printf("eight");
+    }
+    if(a == 9) {
+        printf("nine");
+    }
+}
