b22fef7ed8671cff69d460ca78a20c8f1fc0fcbb
matheusabreus <matheusabreus@hotmail.com>
2021-05-16 22:29:28 -0300

    Create SEMANA07_q12.c

---
+++ SEMANA07_q12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, i, t;
+    scanf("%d", &a);
+   if (a <=100){
+   for(i = 1; i <= a; i++){
+    t = i % 4; 
+    if (t != 0){
+        printf("%d,", i);
+
+    }else{
+                printf("PIN,");
+    }
+  {
+  }}}
+   
+
+}
